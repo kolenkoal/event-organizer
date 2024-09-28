f91927b30fc34bf5b6594e1ee575a627f7385617
kolenkoal <kolenko.alexander0@gmail.com>
2024-09-28 13:34:42 +0300

    remove dishka

--- src/app.py
+++ src/app.py
@@ -1,9 +1,7 @@
-from dishka.integrations.fastapi import setup_dishka
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.routes import auth_router
-from src.ioc.container import get_async_container
 
 
 def create_app() -> FastAPI:
@@ -14,8 +12,6 @@ def create_app() -> FastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
-    dishka_container = get_async_container()
-    setup_dishka(dishka_container, app)
 
     router = APIRouter(prefix="/api/v1")
     router.include_router(auth_router)
--- src/ioc/container.py
+++
@@ -1,7 +0,0 @@
-from dishka import AsyncContainer, make_async_container
-
-from src.ioc.providers.database import get_database_provider
-
-
-def get_async_container() -> AsyncContainer:
-    return make_async_container(get_database_provider())
